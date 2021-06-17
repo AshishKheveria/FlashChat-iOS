313a360c49fc79107d64648ab1fbc4efb3a1f283
AshishKheveria <ashishkheveriaguitz@gmail.com>
2021-06-18 01:47:19 +0530

    First Commit

---
+++ FlashChat/Model/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat
+//
+//  Created by Ashish Kheveria on 08/03/21.
+//  Copyright © 2021 Ashish Kheveria. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String 
+    let body: String
+}
